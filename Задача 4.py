c62c4320c7243d8215a9c5eea052ee6f716ae543
FloR1Plotnikov <73170429+FloR1Plotnikov@users.noreply.github.com>
2020-11-02 22:28:20 +0300

    Add files via upload

---
+++ Задача 4.py
@@ -0,0 +1,7 @@
+import math
+data = 200420
+a = int(input('Ввидите число: '))
+if a==data:
+	print('true')
+else:
+	print('false')
---
+++ Задача5.py
@@ -0,0 +1,6 @@
+x = int(input('Введите х: '))
+y = int(input('Введите у: '))
+if (x>=0) and (x*x+y*y>=9) and (x*x+y*y<=25):
+	print('true')
+else:
+	print('false')
